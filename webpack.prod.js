6d87dfa53186ba3fc7ab4551489819377c0effe9
Adam Gee <adam@adamatronix.com>
2020-09-16 12:19:43 +0800

    remove generated map

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,6 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
    mode: 'production',
-   devtool: 'source-map',
    optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
